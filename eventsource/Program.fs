d9946808e41fc0187971f81602479e26cfb3ea89
Kimserey Lam <kimserey.lam@gmail.com>
2016-03-06 00:28:53 +0000

    Working example

---
+++ eventsource/Program.fs
@@ -0,0 +1,24 @@
+﻿open Suave
+open Suave.Operators
+open Suave.Filters
+open Suave.EventSource
+open Suave.Files
+open Suave.Sockets.Control
+
+
+let app = 
+    choose [ 
+        GET >=> choose [
+            path "/events" >=> request (fun r -> EventSource.handShake(fun out ->
+                socket {
+                    let msg = { id = "1"; data = "First Message"; ``type`` = None }
+                    do! msg |> send out
+                }))
+            browseHome 
+        ]
+    ]
+
+[<EntryPoint>]
+let main argv = 
+    startWebServer { defaultConfig with homeFolder = Some __SOURCE_DIRECTORY__ } app
+    0
